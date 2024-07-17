1e8e4d1605ab0ec51a9b92031c952ea2c0087fee
Nguyen Minh <minhnguyenbk93@gmail.com>
2024-07-17 14:14:48 +0700

    Tai len session 09

---
+++ Sessions10/S10.js
@@ -0,0 +1,55 @@
+// alert("Hello world");
+
+// Input
+// prompt
+
+// prompt("Mời bạn nhập vào tên của bạn:");
+
+// var book="Toi la ai, ai la toi";
+// let film="are you lady";
+// const album="ABC";
+
+// In nhuưng gia tri dang nam trong bien
+// alert(book);
+// alert(film);
+// alert(album);
+
+// book="Harry Potter";
+
+// alert(book);
+
+// film="Friends";
+// alert(film);
+
+// var a=+prompt("Mời bạn nhập vào số a");
+// var b=+prompt("mời bạn nhập vào số b");
+// var S=a+b;
+// alert(S);
+
+
+// let number1=20;
+// let number2=7;
+
+// console.log(number1+number2); 
+
+let sentence = 'Hello " Tôi là Ronaldo';
+let randomString = `ABC123`;
+
+let poem=`Nam Quốc Sơn Hà Nam đế cư
+Tuyệt nhiên định phận tại thiên thư
+Như Hà nghịch lỗ tam thiên xích
+lữ đẳng hành kahn thủ bại hư`
+console.log(randomString);
+let number = 10;
+console.log(number);
+console.log(poem)
+
+let stringA="Xin chào";
+let stringB="Tôi tên là thầy Quang";
+stringA+stringB;
+console.log(stringA+" "+stringB)
+let stringD="thầy A";
+
+let stringC=`Xin chào các bạn, tôi tên là ${stringD}, tôi năm nay hơn 70 tuổi mà chưa gặp trường hợp nào như thế này`
+
+console.log(stringC);
